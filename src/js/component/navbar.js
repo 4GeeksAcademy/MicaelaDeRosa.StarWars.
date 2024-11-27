2195e467a6bdc67b6d14204ad747a4579eb3d49a
MicaelaDeRosa23 <micaaderosa@gmail.com>
2024-11-27 19:00:19 +0000

    MicaelaDeRosa.StarWars

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -5,13 +5,31 @@ export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mb-0 h1">Star Wars</span>
 			</Link>
 			<div className="ml-auto">
 				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+                <div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+    Favorites
+  </button>
+  <ul class="dropdown-menu">
+    <li><a class="dropdown-item" href="#">Add favorite</a></li>
+    <li><a class="dropdown-item" href="#">Delete favorite</a></li>
+  </ul>
+</div>
 				</Link>
 			</div>
 		</nav>
 	);
-};
+};               //     <Link
+//className="text-decoration-none text-dark"
+//to={`/info/${type}/${id}/${img}`}
+//>
+//<p className="hover-effect p-2">{name}</p>
+//</Link>
+//<i
+//className="fas fa-trash me-3 hover-delete"
+//={(event) => {
+  //actions.deleteFavorites(type, id);
+//}}
